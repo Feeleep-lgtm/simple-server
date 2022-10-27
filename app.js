ff26dae76dd32270c3a6d2bbbbf44e348deace7b
feeleep <aliup45@gmail.com>
2022-10-27 21:37:07 +0100

    modified app

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", cors(), (req, res, next) => {
   next();
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("server started at port 3000");
 });
